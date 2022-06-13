5774b13b8999f1755824663d4ee6b2d74aee2a65
Eprst <easter_p@mail.ru>
2022-06-13 16:24:37 +0300

    fix table error

--- src/components/product/tabs.tsx
+++ src/components/product/tabs.tsx
@@ -34,18 +34,20 @@ function Tabs({guitar}: TabsProps): JSX.Element {
       </Link>
       <div className="tabs__content" id="characteristics">
         <table className={`tabs__table ${activeTab === GuitarTabs.Characteristics ? '' : 'hidden'}`}>
-          <tr className="tabs__table-row">
-            <td className="tabs__title">Артикул:</td>
-            <td className="tabs__value">{guitar?.vendorCode}</td>
-          </tr>
-          <tr className="tabs__table-row">
-            <td className="tabs__title">Тип:</td>
-            <td className="tabs__value">{guitarTypeNames[guitar?.type || 'electric']}</td>
-          </tr>
-          <tr className="tabs__table-row">
-            <td className="tabs__title">Количество струн:</td>
-            <td className="tabs__value">{guitar?.stringCount} струнная</td>
-          </tr>
+          <tbody>
+            <tr className="tabs__table-row">
+              <td className="tabs__title">Артикул:</td>
+              <td className="tabs__value">{guitar?.vendorCode}</td>
+            </tr>
+            <tr className="tabs__table-row">
+              <td className="tabs__title">Тип:</td>
+              <td className="tabs__value">{guitarTypeNames[guitar?.type || 'electric']}</td>
+            </tr>
+            <tr className="tabs__table-row">
+              <td className="tabs__title">Количество струн:</td>
+              <td className="tabs__value">{guitar?.stringCount} струнная</td>
+            </tr>
+          </tbody>
         </table>
         <p className={`tabs__product-description ${activeTab === GuitarTabs.Description ? '' : 'hidden'}`}>{guitar?.description}</p>
       </div>
